6556ef9c65ff424a4821a82e756738b5b33a745c
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-10-22 18:28:45 +0200

    Create whisper.py

---
+++ whisper.py
@@ -0,0 +1,16 @@
+import torch
+from optimum.pipelines import pipeline
+from datasets import load_dataset
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Device: ", device)
+pipe = pipeline(
+  "automatic-speech-recognition",
+  model="openai/whisper-large-v2",
+  chunk_length_s=30,
+  device=device,
+  accelerator="ort"
+)
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+sample = ds[0]["audio"]
+prediction = pipe(sample.copy(), batch_size=8)["text"]
