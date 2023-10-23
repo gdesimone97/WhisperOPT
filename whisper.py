50e36a26e50b3400eb2eb46bd96c7d975ff53126
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-10-23 09:46:25 +0200

    up

--- whisper.py
+++ whisper.py
@@ -1,8 +1,10 @@
 import torch
 from optimum.pipelines import pipeline
 from datasets import load_dataset
+from pathlib import Path
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+curr_dir = Path(__file__).parent
 print("Device: ", device)
 pipe = pipeline(
   "automatic-speech-recognition",
@@ -10,7 +12,7 @@ pipe = pipeline(
   chunk_length_s=30,
   device="cpu",
   accelerator="ort",
-  cache_dir=r"S:\MIE CARTELLE\PROGRAMMAZIONE\GITHUB\WhisperOPT"
+  cache_dir=curr_dir
 )
 ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
 sample = ds[0]["audio"]
