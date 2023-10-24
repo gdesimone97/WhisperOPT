9da02eab81ecd3eb5a9499c5481c5fadc7be8a93
gdesimone <gds2011@libero.it>
2023-10-24 08:13:00 +0000

    up test

--- test.py
+++ test.py
@@ -6,13 +6,16 @@ from pathlib import Path
 # Number of inferences for comparing timings
 num_inferences = 4
 save_dir = Path(__file__).parent.joinpath("whisper-large") #complete here
-inference_file =  Path(__file__).parent.joinpath("") #complete here
+inference_file =  Path(__file__).parent.joinpath("test.wav") #complete here
+if not inference_file.exists():
+    raise Exception(f"File: {inference_file} doesn't exist")
 
+print("Starting")
 # Create pipeline based on quantized ONNX model
 model = ORTModelForSpeechSeq2Seq.from_pretrained(save_dir)
 tokenizer = AutoTokenizer.from_pretrained(save_dir)
 feature_extractor = AutoFeatureExtractor.from_pretrained(save_dir)
-cls_pipeline_onnx = pipeline("automatic-speech-recognition", model=model, tokenizer=tokenizer, feature_extractor=feature_extractor)
+cls_pipeline_onnx = pipeline("automatic-speech-recognition", model=model, tokenizer=tokenizer, feature_extractor=feature_extractor, accelerator="ort")
 
 start_quantized = datetime.now()
 for i in range(num_inferences):
