79e86c4b77a02a3ea9598aed0ac0125cbbe088f1
gdesimone <gds2011@libero.it>
2023-10-24 09:11:56 +0000

    up tests

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 from datetime import datetime
+from scipy.io import wavfile
 from optimum.onnxruntime import ORTModelForSpeechSeq2Seq
 from transformers import pipeline, AutoTokenizer, AutoFeatureExtractor
 from pathlib import Path
 
 # Number of inferences for comparing timings
 num_inferences = 4
-save_dir = Path(__file__).parent.joinpath("whisper-large") #complete here
-inference_file =  Path(__file__).parent.joinpath("test.wav") #complete here
-if not inference_file.exists():
-    raise Exception(f"File: {inference_file} doesn't exist")
+save_dir = Path(__file__).parent.joinpath("whisper-large-no_conv") #complete here
+inference_file_path =  Path(__file__).parent.joinpath("test.wav") #complete here
+if not inference_file_path.exists():
+    raise Exception(f"File: {inference_file_path} doesn't exist")
+_, inference_file =  wavfile.read(inference_file_path)
 
-print("Starting")
+print("Loading model...")
 # Create pipeline based on quantized ONNX model
 model = ORTModelForSpeechSeq2Seq.from_pretrained(save_dir)
 tokenizer = AutoTokenizer.from_pretrained(save_dir)
 feature_extractor = AutoFeatureExtractor.from_pretrained(save_dir)
-cls_pipeline_onnx = pipeline("automatic-speech-recognition", model=model, tokenizer=tokenizer, feature_extractor=feature_extractor, accelerator="ort")
+cls_pipeline_onnx = pipeline("automatic-speech-recognition", model=model, tokenizer=tokenizer, feature_extractor=feature_extractor)
 
+print("Starting inference...")
 start_quantized = datetime.now()
 for i in range(num_inferences):
     cls_pipeline_onnx(inference_file)
