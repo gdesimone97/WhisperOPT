d418139632583de8b5cc7ce6493cd50c86fd8bce
gdesimone <gds2011@libero.it>
2023-11-02 20:10:27 +0000

    better

--- quantize_static.py
+++ quantize_static.py
@@ -3,6 +3,7 @@ from functools import partial
 from transformers import AutoTokenizer
 from optimum.onnxruntime import (
     AutoQuantizationConfig,
+    AutoCalibrationConfig,
     ORTModelForSpeechSeq2Seq,
     ORTQuantizer
 )
@@ -15,7 +16,6 @@ cache_dir = Path(__file__).parent.joinpath(f'{model_id.split("/")[1]}')
 # Export model in ONNX
 model = ORTModelForSpeechSeq2Seq.from_pretrained(model_id, export=True, cache_dir=cache_dir)
 tokenizer = AutoTokenizer.from_pretrained(model_id) #tokenaizer
-quantizer = ORTQuantizer.from_pretrained(model)
 qconfig = AutoQuantizationConfig.tensorrt(per_channel=False) #quantization config
 print("Model loaded")
 model_dir = model.model_save_dir
@@ -29,20 +29,22 @@ quantizers = [ORTQuantizer.from_pretrained(model_dir, file_name=onnx_model) for 
 def preprocess_fn(ex, tokenizer):
     return tokenizer(ex["sentence"])
 
-calibration_dataset = quantizer.get_calibration_dataset(
+for quantizer in quantizers:
+    # Apply dynamic quantization and save the resulting model
+    calibration_dataset = quantizer.get_calibration_dataset(
     "hf-internal-testing/librispeech_asr_dummy",
+    "clean",
     preprocess_function=partial(preprocess_fn, tokenizer=tokenizer),
     num_samples=250,
     dataset_split="train",
-)
+    )
 
-calibration_config = AutoCalibrationConfig.minmax(calibration_dataset) #calibration config
-ranges = quantizer.fit(
-    dataset=calibration_dataset,
-    calibration_config=calibration_config,
-    operators_to_quantize=qconfig.operators_to_quantize,
-)
-
-for quantizer in quantizers:
-    # Apply dynamic quantization and save the resulting model
-    quantizer.quantize(save_dir=save_dir, quantization_config=qconfig)
+    calibration_config = AutoCalibrationConfig.minmax(calibration_dataset) #calibration config
+    ranges = quantizer.fit(
+        dataset=calibration_dataset,
+        calibration_config=calibration_config,
+        operators_to_quantize=qconfig.operators_to_quantize,
+    )
+    quantizer.quantize(save_dir=save_dir, 
+                       quantization_config=qconfig,
+                       calibration_tensors_range=ranges)
