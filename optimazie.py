cd2a78731dc269420d789b91fabdbf4fb77db008
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-11-03 17:01:23 +0100

    up

--- optimazie.py
+++ optimazie.py
@@ -1,4 +1,17 @@
+from pathlib import Path
+
 from optimum.onnxruntime import (
-    AutoOptimizationConfig, ORTOptimizer, ORTModelForSequenceClassification
+    AutoOptimizationConfig, ORTOptimizer, ORTModelForSpeechSeq2Seq
 )
 
+curr_dir = Path(__file__).parent
+model_id = "openai/whisper-large-v2"
+model_name = model_id.split("/")[1]
+save_dir = curr_dir.joinpath((model_name + "-optimized"))
+
+model = ORTModelForSpeechSeq2Seq.from_pretrained(model_id, export=True, 
+                                                 cache_dir=curr_dir.joinpath((model_name + "-cache")))
+
+optimizer = ORTOptimizer.from_pretrained(model)
+optimization_config = AutoOptimizationConfig.O4()
+optimizer.optimize(save_dir=save_dir, optimization_config=optimization_config)
