cd7dc168e67c0ffb73a5628f0341013a89ccc766
gdesimone <gds2011@libero.it>
2023-11-05 15:15:28 +0000

    vars

--- export.bash
+++ export.bash
@@ -1,5 +1,5 @@
-export CUDA_HOME=/usr/local/cuda-12.0
-export CUDA_PATH=/usr/local/cuda
 export LD_LIBRARY_PATH=/usr/local/lib/python3.8/dist-packages/nvidia/cublas/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.8/dist-packages/nvidia/cufft/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.8/dist-packages/nvidia/cuda_runtime/lib/
 
 optimum-cli export onnx --model openai/whisper-large-v2 --optimize O4 --device cuda whisper-large-v2-optimized/
