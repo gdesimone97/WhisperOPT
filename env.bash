d33dc53e2443596eed05d9d5f410eacd96021a61
gdesimone <gds2011@libero.it>
2023-11-06 09:00:44 +0000

    cuda envs

---
+++ env.bash
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/usr/local/lib/python3.8/dist-packages/nvidia/cublas/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.8/dist-packages/nvidia/cufft/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.8/dist-packages/nvidia/cuda_runtime/lib/
