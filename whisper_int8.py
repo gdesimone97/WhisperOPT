0bc6bf135d37d1f47e244e8cb06cb46a121de902
gdesimone <gds2011@libero.it>
2025-05-02 17:39:14 +0200

    ups

--- whisper_int8.py
+++ whisper_int8.py
@@ -50,36 +50,40 @@ def forward_encoder(model):
                            truncation=True,
                            return_attention_mask=True)
         inputs = {k: v.to("cuda") for k, v in inputs.items()}
+        inputs["input_features"] = inputs["input_features"].to(encoder.dtype)
         out = model(**inputs)
         last_hidden_state = out.last_hidden_state
-        torch.save(
-            last_hidden_state, 
-            out_tensors_path.joinpath(f"last_hidden_{i}.pt")
-        )
         bar.update(1)
         NUM_STEPS = i
         
 def forward_decoder(model):
+    global NUM_STEPS
+    batch_size = 1
+    dataset_iter = dataset.iter(batch_size=batch_size)
     bar = tqdm(total=MAX_ITER, desc="Calibrating", unit="batch")
     decoder_prompt_ids = processor(text="", return_tensors="pt")["input_ids"][..., :-1]
-    eos = processor.tokenizer.eos_token_id
-    new_token = None
-    for i in range(NUM_STEPS):
-        last_hidden_state = torch.load(
-            out_tensors_path.joinpath(f"last_hidden_{i}.pt")
-        ).to("cuda")
-        while new_token is None or (new_token != eos and j < 400):
-            j = 0
-            inputs = {}
-            inputs["input_ids"] = decoder_prompt_ids
-            inputs["encoder_hidden_states"] = last_hidden_state
-            inputs = {k: v.to("cuda") for k, v in inputs.items()}
-            logits = model(**inputs)
-            new_token = torch.argmax(logits, dim=-1)[:, -1].unsqueeze(0)
-            decoder_prompt_ids = torch.cat([decoder_prompt_ids.to("cuda"), new_token], dim=-1)
-            out_text = processor.batch_decode(decoder_prompt_ids, skip_special_tokens=False)
-            j += 1
-        print(f"Decoded text: {out_text}")
+    for i, sample in enumerate(dataset_iter):
+        sample = sample["audio"]
+        if i >= MAX_ITER:
+            break
+        sr = [s["sampling_rate"] for s in sample]
+        sr_0 = sr[0]
+        assert np.all(np.array(sr) == sr_0), "All samples must have the same sampling rate"
+        audio = [s["array"] for s in sample]
+        inputs = processor(audio=audio, 
+                           sampling_rate=sr_0, 
+                           return_tensors="pt",
+                           truncation=True,
+                           return_attention_mask=True)
+        inputs = {k: v.to("cuda") for k, v in inputs.items()}
+        inputs["input_features"] = inputs["input_features"].to(encoder.dtype)
+        out = encoder(**inputs)
+        last_hidden_state = out.last_hidden_state
+        inputs = {}
+        inputs["input_ids"] = decoder_prompt_ids
+        inputs["encoder_hidden_states"] = last_hidden_state
+        inputs = {k: v.to("cuda") for k, v in inputs.items()}
+        logits = model(**inputs)
         bar.update(1)
 
 if __name__ == "__main__":
@@ -100,28 +104,31 @@ if __name__ == "__main__":
     out_decoder = Path("onnx_model_decoder").joinpath(fname_decoder)
     out_decoder.parent.mkdir(parents=True, exist_ok=True)
     
-    model_name = "openai/whisper-large-v3"
+    model_name = "openai/whisper-large-v3-turbo"
     
     dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
     
+    dtype = torch.float16
     whisper_model = WhisperForConditionalGeneration.from_pretrained(model_name,
-                                                              torch_dtype=torch.float32,
+                                                              torch_dtype=dtype,
                                                               device_map="cuda",
                                                               )
     encoder = whisper_model.get_encoder()
+    assert encoder.dtype == dtype, f"Encoder dtype {encoder.dtype} does not match model dtype {dtype}"
     whisper_model = WhisperForConditionalGeneration.from_pretrained(model_name,
-                                                              torch_dtype=torch.float32,
+                                                              torch_dtype=dtype,
                                                               device_map="cuda",
                                                               )
     decoder = whisper_model.get_decoder()
+    assert decoder.dtype == dtype, f"Decoder dtype {decoder.dtype} does not match model dtype {dtype}"
     decoder = Decoder(decoder, whisper_model.proj_out).to("cuda")
     processor = WhisperProcessor.from_pretrained(model_name)
     processor.tokenizer.set_prefix_tokens(language="en", task="transcribe")
     config = mtq.INT8_DEFAULT_CFG
-    dummy = torch.randn(1, 128, 3000).to("cuda")
-    
-    encoder = mtq.quantize(encoder, config, forward_encoder)
+    dummy = torch.randn(1, 128, 3000).to("cuda", dtype=encoder.dtype)
+
     decoder = mtq.quantize(decoder, config, forward_decoder)
+    encoder = mtq.quantize(encoder, config, forward_encoder)
     shutil.rmtree(out_tensors_path)
     
     dynamic_axes = {
@@ -151,7 +158,7 @@ if __name__ == "__main__":
     decoder_prompt_ids = processor.get_decoder_prompt_ids()
     decoder_prompt_ids = [p[1] for p in decoder_prompt_ids]
     decoder_prompt_ids = torch.LongTensor(decoder_prompt_ids).unsqueeze(0).to("cuda")
-    enc_output = encoder(dummy).last_hidden_state.to("cuda")   
+    enc_output = encoder(dummy).last_hidden_state.to("cuda", dtype=dtype)   
     dummy = {
         "input_ids": decoder_prompt_ids, 
         "encoder_hidden_states": enc_output,
